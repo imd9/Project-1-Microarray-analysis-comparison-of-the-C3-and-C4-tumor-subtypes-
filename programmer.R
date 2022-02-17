1b54defbf7264552f84bf0dc728242922e1fc049
Preshita Sanjay Dave <52149025+PreshitaDave@users.noreply.github.com>
2022-02-16 21:48:38 -0500

    Updated PCA plot

--- programmer.R
+++ programmer.R
@@ -5,6 +5,7 @@ library(sva)
 library(AnnotationDbi)
 library(hgu133plus2.db)
 library(ggplot2)
+library(dplyr)
 
 # loading the data by setting the working directory
 setwd('/usr4/bf527/preshita/project-1-swisscheese/samples')
@@ -25,14 +26,15 @@ data.matrix = exprs(data.rma)
 # https://www.bioconductor.org/packages/devel/bioc/vignettes/affyPLM/inst/doc/QualityAssess.pdf
 Pset <- fitPLM(data, normalize=TRUE, background=TRUE)
 
-
-
 stats_RLE<-data.frame(RLE(Pset,type="stats"))
 stats_RLE<-as.data.frame(t(stats_RLE))
 jpeg("hist_RLE.jpg")
 hist(stats_RLE$median, xlab="median RLE values", main = "Histogram plot of median RLE values", col='lightblue')
 dev.off()
 
+#getting outliers based on RLE plot
+outliers_RLE<-stats_RLE %>% filter(median>0.1)
+row.names(outliers_RLE)
 
 stats_NUSE<-data.frame(NUSE(Pset,type="stats"))
 stats_NUSE<-as.data.frame(t(stats_NUSE))
@@ -40,19 +42,25 @@ jpeg("hist_NUSE.jpg")
 hist(stats_NUSE$median, xlab="median NUSE values", main = "Histogram plot of median NUSE values", col = 'lightgreen')
 dev.off()
 
+#getting outliers based on NUSE plot
+outliers_NUSE<-stats_NUSE %>% filter(median>1.05)
+row.names(outliers_NUSE)
+
 # Combat for batch effect correction 
 metadata=read.csv('/project/bf528/project_1/doc/proj_metadata.csv')
 batch_effect_correction <- ComBat(dat = data.matrix, batch = metadata$normalizationcombatbatch, mod = model.matrix(~as.factor(metadata$normalizationcombatmod), data=metadata))
 write.table(batch_effect_correction, file="/usr4/bf527/preshita/project-1-swisscheese/corrected_batch_values.csv")
 
 
-
 #PCA 
 scaled_data <- t(scale(t(batch_effect_correction)))
 pca_output<-prcomp(scaled_data, scale=FALSE, center=FALSE)
 rotated_pca<-as.data.frame(pca_output$rotation)
-summary(pca_output) #helps to find out percent variability. Don't know about $importance attribute and what it falls under
+pca_df <- data.frame(PC1=rotated_pca[,1], PC2=rotated_pca[,2], Cancer.Subtype=metadata$SixSubtypesClassification)
 
+summary(pca_output) #helps to find out percent variability. 
+
+#preparing x and y labels 
 var1<-as.character(summary(pca_output)$importance[2,1]*100)
 var2<-as.character(summary(pca_output)$importance[2,2]*100)
 xlabel <- c("PC1",var1)
@@ -61,5 +69,5 @@ ylabel <- c("PC2",var2)
 ylabel <- toString(ylabel)
 
 jpeg("pcaplot.jpg")
-ggplot(data = rotated_pca, mapping = aes(x = PC1, y = PC2)) +geom_point()+labs( x= xlabel, y=ylabel)+ggtitle("PCA Result")+theme(plot.title = element_text(hjust = 0.5))
+ggplot(data = pca_df, mapping = aes(x = PC1, y = PC2, col=Cancer.Subtype, fill=Cancer.Subtype)) +geom_point()+labs( x= xlabel, y=ylabel)+ggtitle("PCA Result")+theme(plot.title = element_text(hjust = 0.5))
 dev.off()
